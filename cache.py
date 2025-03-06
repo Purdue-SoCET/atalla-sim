71457e9dde0003a3479946a75912d2bac3e42b8a
socet90 <socet90@asicfab.ecn.purdue.edu>
2025-03-05 20:37:54 -0500

    First attempt at non-block with seemingly functional non-blocking reads

--- cache.py
+++ cache.py
@@ -4,12 +4,13 @@ from helpers import tobits, frombits
 
 WORD_SIZE = 32
 BYTES_PER_WORD = WORD_SIZE//8
-
-CS          = 1024*4# Cache size in bits
-BLOCK_SIZE  = 4     # Block size in words
-A           = 4     # Associativity
-BANKS       = 4     # Number of banks
 ADDR_BITS   = 32    # Address bits
+
+CS          = 1024*2 # Cache size in bits
+BLOCK_SIZE  = 2      # Block size in words
+A           = 2      # Associativity
+BANKS       = 4      # Number of banks
+K           = 8      # MSHR size
 
 class CacheFrame:
     def __init__(self, tag = 0, data = None, address = None, tick = -1):
@@ -26,6 +27,25 @@ class CacheFrame:
             data_str = str(frombits(self.data[word_idx])).ljust(4, ' ') + data_str
         return valid_str + f" Tag: {self.tag}   Data: {data_str}   Addr: {hex(self.address)}   Tick: {self.lru_tick}"
 
+class MSHREntry:
+    def __init__(self, address, cache, rw_mode : int, data = None):
+        self.valid = True
+        self.uuid = 0
+        self.write_status = BLOCK_SIZE * [0]
+        self.write_block = [[0 for _ in range(WORD_SIZE)] for _ in range(BLOCK_SIZE)]
+        self.add_miss(address, cache, rw_mode, data)
+
+    def add_miss(self, address, cache, rw_mode : int, data = None):
+        self.address = address
+        tag, idx, blk_off, bank = cache.extract_info(address)
+        # self.write_status[blk_off] = rw_mode
+        # if rw_mode:
+        #     self.write_block[blk_off] = data
+        self.blk_off = blk_off
+        self.bank = bank
+        self.idx = idx
+        self.tag = tag
+
 class Cache:
     def __init__(self, dram = None):
         self.NUM_TOTAL_WORDS = (CS // 8) // BYTES_PER_WORD
@@ -37,7 +57,7 @@ class Cache:
         print("Banks:", BANKS)
         print("-> Sets:", self.NUM_SETS_PER_BANK)
         print("   -> Frames:", self.NUM_FRAMES_PER_SET)     
-        print("      -> Blocks:", BLOCK_SIZE)
+        print("      -> Words:", BLOCK_SIZE)
         print("         -> Bits:", WORD_SIZE)
         
         
@@ -53,7 +73,10 @@ class Cache:
         print("Bank idx. bits : ", self.BANK_BITS)
         print("Tag bits       : ", self.TAG_BITS)
         self.cache = [[[CacheFrame() for _ in range(A)] for _ in range(self.NUM_SETS_PER_BANK)] for _ in range(self.NUM_BANKS)]
+        self.bank_latencies = self.NUM_BANKS * [0]
         self.dram = dram
+        self.mshr = []
+        self.data_out = {}
 
     def extract_info(self, address):
         address = tobits([address], bit_len=ADDR_BITS)
@@ -63,6 +86,49 @@ class Cache:
         idx     = frombits(address[2 + self.BANK_BITS + self.BLKOFF_BITS: ADDR_BITS - self.TAG_BITS])
         tag     = frombits(address[ADDR_BITS - self.TAG_BITS: ])
         return tag, idx, blk_off, bank
+
+    def main_loop(self, tick):
+        status_str = f"--- Tick {tick}   MSHR: {len(self.mshr)}/{K}"
+        for b_i, bank in enumerate(self.cache):
+            status_str += f"  B{b_i}: +{self.bank_latencies[b_i]}".ljust(4, ' ')
+        print(status_str)
+
+        for b_i, banks in enumerate(self.cache):
+            if self.bank_latencies[b_i]:
+                self.bank_latencies[b_i] -= 1
+        if len(self.mshr) == 0:
+            return None
+        entry = self.mshr[0]
+        if self.bank_latencies[entry.bank] != 0:
+            return None
+        for frame in self.cache[entry.bank][entry.idx]:
+            if frame.valid and entry.tag == frame.tag:
+                data_out = deepcopy(frame.data[entry.blk_off])
+                frame.lru = tick
+                Cache.print_message("~ Hit      :", entry.address, entry.tag, entry.idx, entry.blk_off, entry.bank)
+                self.mshr.pop(0)
+                self.data_out[entry.address] = data_out
+                return
+        else:
+            Cache.print_message("~ Miss     :", entry.address, entry.tag, entry.idx, entry.blk_off, entry.bank)
+            self.mshr.pop(0)
+            self.bank_latencies[entry.bank] = 20
+            data_out = deepcopy(self.dram[entry.address])
+            for word_idx in range(BLOCK_SIZE):
+                dram_word_addr  = entry.bank << 2
+                dram_word_addr += word_idx   << (2 + self.BANK_BITS)
+                dram_word_addr += entry.idx  << (2 + self.BANK_BITS + self.BLKOFF_BITS)
+                dram_word_addr += entry.tag  << (ADDR_BITS - self.TAG_BITS)
+                data_from_dram = self.dram[dram_word_addr]
+                self.write(dram_word_addr, data_from_dram, tick)
+            self.data_out[entry.address] = data_out
+            
+
+    def request_read(self, address, tick):
+        if len(self.mshr) == K:
+            return False
+        self.mshr.append(MSHREntry(address, self, 0))
+        return True
 
     def read(self, address, tick):
         tag, idx, blk_off, bank = self.extract_info(address)
@@ -77,10 +143,10 @@ class Cache:
             Cache.print_message("~ Miss     :", address, tag, idx, blk_off, bank)
             data = deepcopy(self.dram[address])
             for word_idx in range(BLOCK_SIZE):
-                dram_word_addr  = bank          << 2
-                dram_word_addr += word_idx      << (2 + self.BANK_BITS)
-                dram_word_addr += idx           << (2 + self.BANK_BITS + self.BLKOFF_BITS)
-                dram_word_addr += tag << (ADDR_BITS - self.TAG_BITS)
+                dram_word_addr  = bank     << 2
+                dram_word_addr += word_idx << (2 + self.BANK_BITS)
+                dram_word_addr += idx      << (2 + self.BANK_BITS + self.BLKOFF_BITS)
+                dram_word_addr += tag      << (ADDR_BITS - self.TAG_BITS)
                 data_from_dram = self.dram[dram_word_addr]
                 self.write(dram_word_addr, data_from_dram, tick)
             return data
@@ -121,7 +187,7 @@ class Cache:
 
     @staticmethod
     def print_message(message, address, tag, idx, blkoffset, bank):
-        print(f"{message} at addr. "+ str(hex(address)).ljust(5, ' ') + f" -> tag {tag} set {idx} blk {blkoffset} bank {bank}")
+        print(f"{message} at addr. "+ str(hex(address)).ljust(5, ' ') + f" -> tag {tag} set {idx} word {blkoffset} bank {bank}")
 
     def __repr__(self):
         str_out = ""
@@ -161,14 +227,29 @@ if __name__ == "__main__":
     for addr, data in sorted(dram.items()):
         print(hex(addr).ljust(5, ' '), frombits(data))
 
-    print("Read test")
-    for i in range(num_test - 1, -1, -1):
-        assert i == frombits(dcache.read(i * n, tick)), f"Incorrect read at address {hex(i * n)}"
-        tick += 1
+    if False:
+        print("Read test")
+        for i in range(num_test - 1, -1, -1):
+            assert i == frombits(dcache.read(i * n, tick)), f"Incorrect read at address {hex(i * n)}"
+            tick += 1
+    else:
+        print("Non-blocking Read test")
+        i_in  = num_test - 1
+        i_out = num_test - 1
+        while i_out >= 0:
+            if i_in >= 0:
+                if dcache.request_read(i_in * n, tick):
+                    i_in -= 1
+            data = dcache.main_loop(tick)
+            if i_out * n in dcache.data_out.keys():
+                assert i_out == frombits(dcache.data_out[i_out * n]), f"Incorrect read at address {hex(i_out * n)}"
+                # print("Expected:", i_out, "   out: ", frombits(dcache.data_out[i_out * n]))
+                i_out -= 1
+            tick += 1
 
-    print("Cache after read test")
-    print(dcache)
+    # print("Cache after read test")
+    # print(dcache)
 
-    print("DRAM after read test")
-    for addr, data in sorted(dram.items()):
-        print(addr, frombits(data))
+    # print("DRAM after read test")
+    # for addr, data in sorted(dram.items()):
+    #     print(hex(addr).ljust(5, ' '), frombits(data))
